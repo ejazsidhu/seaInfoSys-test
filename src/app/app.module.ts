58a8513e5a50bebe9e67a307e82d374ee07770c0
ejazsidhu <ejazsidhu@gmail.com>
2018-11-30 23:11:42 +0500

    folder structer created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './public/login/login.component';
+import { ContactsComponent } from './public/contacts/contacts.component';
+import { ShowsComponent } from './private/shows/shows.component';
+import { ShowListComponent } from './private/shows/show-list/show-list.component';
+import { ShowDetailComponent } from './private/shows/show-detail/show-detail.component';
+import { TopBarComponent } from './private/layout/top-bar/top-bar.component';
+import { FooterComponent } from './private/layout/footer/footer.component';
+import { ContentComponent } from './private/layout/content/content.component';
+import { HomeComponent } from './private/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContactsComponent,
+    ShowsComponent,
+    ShowListComponent,
+    ShowDetailComponent,
+    TopBarComponent,
+    FooterComponent,
+    ContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
