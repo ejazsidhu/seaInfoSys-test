5c8d6a7a3a26fa71d5f8936791702daae1772db8
ejazsidhu <ejazsidhu@gmail.com>
2018-12-01 22:03:23 +0500

    contactpage added

--- src/app/public/contacts/contacts.component.ts
+++ src/app/public/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EmailValidator } from 'src/assets/validators/email.validator';
 
 @Component({
   selector: 'app-contacts',
@@ -8,12 +9,35 @@ import { FormGroup } from '@angular/forms';
 })
 export class ContactsComponent implements OnInit {
 
-  public form:FormGroup;
-  
+  public form: FormGroup;
 
-  constructor() { }
+  public message = '';
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, EmailValidator.validate, Validators.minLength(4)])],
+      subject: ['', Validators.compose([Validators.required])],
+      body: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  onSubmit(form) {
+
+    if (form.valid) {
+      setTimeout(() => {
+        this.form.reset();
+        this.message = "Contact added successfully";
+
+        setTimeout(() => {
+          this.message='';
+          
+        }, 3000);
+
+      }, 3000);
+    }
+
   }
 
 }
